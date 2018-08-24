2f03e0fe091fbffa009b73de55a8f80a738f54b3
RiverHeart <RiverHeart@users.noreply.github.com>
2018-08-23 23:27:25 -0400

    Added modules directory. Adding SortingAlgo module.

---
+++ modules/SortingAlgorithms/SortingAlgorithms/SortingAlgorithms.psm1
@@ -0,0 +1,16 @@
+﻿Write-Verbose "Importing Functions"
+
+# Import everything in subfolders folder
+foreach ( $folder in @( 'private', 'public', 'classes' ) )
+{
+    $root = Join-Path -Path $PSScriptRoot -ChildPath $folder
+    if (Test-Path -Path $root)
+    {
+        Write-Verbose "Processing folder $root"
+        $files = Get-ChildItem -Path $root -Filter *.ps1
+
+        # dot source each file
+        $files | Where-Object { $_.Name -notlike '*.Tests.ps1' } |
+            ForEach-Object { Write-Verbose $_.Name; . $_.FullName }
+    }
+}
